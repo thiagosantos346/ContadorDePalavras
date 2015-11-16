557ba1430883f149cdc27746e298c39b68cf8c9e
thiag <thiag@192.168.25.36>
2015-11-16 20:34:27 -0200

    fazer coreções na linha 111

--- src/com/zero/ManipuladorPalavras.java
+++ src/com/zero/ManipuladorPalavras.java
@@ -29,13 +29,13 @@ public class ManipuladorPalavras {
         String[] listaPalavras = param;
 
         String[] novaListaPalavras = new String[t];
-        ArrayList<String> newArray = new ArrayList(); 
+        ArrayList<String> newArray = new ArrayList();
         ArrayList<Integer> newArrayId = new ArrayList();
 
         Integer[] quantidadeItemPalvra = new Integer[t];
 
-        ArrayList<String> tabela = new ArrayList();
-        ArrayList<String> quantidade = new ArrayList();
+        //ArrayList<String> tabela = new ArrayList();
+        //ArrayList<String> quantidade = new ArrayList();
 
         for (int r = 0; r < t; r++) {
 
@@ -46,26 +46,24 @@ public class ManipuladorPalavras {
 
         for (i = 0; i < listaPalavras.length; i++) {
             try {
-                
+
                 boolean acho = false;
-                
+
                 if (listaPalavras[i] != null) {
-                    
-                    for (j = 0 ; j < listaPalavras.length; j++) {
+
+                    for (j = 0; j < listaPalavras.length; j++) {
                         newArray.add("");
                         if (listaPalavras[i].equals(listaPalavras[j])) {
-                            
+
                             l = 0;
 
                             while ((l < listaPalavras.length) && (acho == false)) {
 
-                               
-                                
                                 if (!newArray.get(j).equals(novaListaPalavras[l]) && acho == false) {
 
                                     //novaListaPalavras[j] = listaPalavras[j];
                                     newArray.add(listaPalavras[j]);
-                                    
+
                                     acho = true;
 
                                 }
@@ -80,46 +78,48 @@ public class ManipuladorPalavras {
                 }
             } catch (NullPointerException e) {
 
-                System.out.println(e.getMessage()+": Line 75\n "+e.getLocalizedMessage());
+                System.out.println(e.getMessage() + ": Line 75\n " + e.getLocalizedMessage());
 
             }
         }
 
         for (int a = 0; a < newArray.size(); a++) {
             int add = 0;
-            for(int b = 0; b < listaPalavras.length; b++) {
+            for (int b = 0; b < listaPalavras.length; b++) {
 
                 try {
                     if (newArray.get(a).equals(listaPalavras[b]) == true) {
 
                         //quantidadeItemPalvra[a]++;
-                        newArrayId.add(add++);
+                        //newArrayId.add(add++);
+                       // newArrayId.add(add++, a );
+                        add++;
 
                     }
                 } catch (NullPointerException e) {
                     System.out.print(e.getMessage());
                 }
-
+                
+                if(a == (newArray.size()) - 1)
+                {
+                    newArrayId.add(add);
+                }
             }
 
         }
 
-        for (int n = 0; n < listaPalavras.length; n++) {
-            if (!(novaListaPalavras[n] == null)) {
+        for (int n = 0; n < newArrayId.size()-1; n++) {
+            try {
 
-                try {
+                //tabela.add(newArray.get(n-1));
+                //quantidade.add(newArrayId.get(n-1).toString());
 
-                    tabela.add( newArray.get(n));
-                    quantidade.add( newArrayId.get(n).toString());
+                formulario.setPalavra(newArray.get(n));
+                formulario.setQuantidade(newArrayId.get(n));
 
-                    formulario.setPalavra(newArray.get(n));
-                    formulario.setQuantidade(newArrayId.get(n));
+            } catch (NullPointerException e) {
 
-                } catch (NullPointerException e) {
-
-                    System.out.printf(e.getMessage());
-
-                }
+                System.out.printf(e.getMessage());
 
             }
 
